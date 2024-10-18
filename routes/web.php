1e0fea93ea1f73abe712bb6492199c744fb6e234
Murat Sarı <ms@muratsari.com.tr>
2024-10-19 02:46:01 +0300

    📚 Redirect index page to API Documentation page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(config('l5-swagger.documentations.default.routes.api'));
 });
